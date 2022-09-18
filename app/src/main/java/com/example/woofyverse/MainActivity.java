d4d2b574155e455a49117cb3fff0648e07f2f751
Arshroop Singh <arshroopsaini@gmail.com>
2022-09-18 17:34:19 +0530

    Adding files via initial commit

---
+++ app/src/main/java/com/example/woofyverse/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.woofyverse;
+
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.annotation.SuppressLint;
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.view.KeyEvent;
+        import android.view.Window;
+        import android.view.WindowManager;
+        import android.webkit.WebResourceRequest;
+        import android.webkit.WebView;
+        import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+        CustomWebViewClient client = new CustomWebViewClient(this);
+
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(client);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://woofyverse.herokuapp.com/");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if(keyCode == KeyEvent.KEYCODE_BACK && this.webView.canGoBack()){
+            this.webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
+
+class CustomWebViewClient extends WebViewClient{
+    private Activity activity;
+    public CustomWebViewClient(Activity activity){
+        this.activity = activity;
+    }
+
+    // API Level less than 24
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, String url){
+        return false;
+    }
+
+    //API level >=24
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, WebResourceRequest request){
+        return false;
+    }
+}
